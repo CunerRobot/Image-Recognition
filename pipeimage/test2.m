e530f4ad5900231d4850651e0cd500534bc91df0
CunerRobot <56438202+CunerRobot@users.noreply.github.com>
2019-11-06 20:07:30 +0800

    Update test2.m

--- pipeimage/test2.m
+++ pipeimage/test2.m
@@ -1,12 +1,12 @@
 clc
 clear all
 close all
-%Get training images
+%% Get training images
 pipe_ds = imageDatastore('pipeimage','IncludeSubfolders',true,'LabelSource','foldernames');%创建带标签的数据库
 %auds=augmentedImageDatastore([227,227],pipe_ds);%创建增强图片数据库，图像尺寸为227x227
-[trainImgs,testImgs] = splitEachLabel(pipe_ds,0.6);%数据库分割
+[trainImgs,ValidationImgs] = splitEachLabel(pipe_ds,0.8,'randomize');%数据库分割
 train_auds=augmentedImageDatastore([227,227],trainImgs);%创建增强图片数据库，图像尺寸为227x227
-test_auds=augmentedImageDatastore([227,227],testImgs);%创建增强图片数据库，图像尺寸为227x227
+Validation_auds=augmentedImageDatastore([227,227],ValidationImgs);%创建增强图片数据库，图像尺寸为227x227
 numClasses = numel(categories(pipe_ds.Labels));%计数分类种类数目
 %Create a network by modifying AlexNet
 net = alexnet;%载入alex网络，一种预训练网络
@@ -18,27 +18,20 @@ layers(end) = classificationLayer;%修改最后一层为分类层
 options = trainingOptions('sgdm',...%使用SGDM优化器
     'InitialLearnRate',0.001,...%学习率为0.001
     'MaxEpochs',30,'MiniBatchSize',16,...%最大循环30，最小批量为16
+    'ValidationData', Validation_auds,...%训练过程中验证
     'ValidationFrequency',5,...%验证指标评估之间的迭代次数
     'Plots','training-progress');%显示训练过程
                                                                                                                           %最小批量为16，显示训练过程
 %Perform training
-[pipenet,info] = trainNetwork(train_auds, layers, options);%用训练集、修改后的训练网络，训练好的网络为flowernet；
+[pipenet,info] = trainNetwork(train_auds,layers, options);%用训练集、修改后的训练网络，训练好的网络为flowernet；
 %Use trained network to classify test images
-testpreds = classify(pipenet,test_auds);%用pipenet对testImgs分类测试
-subplot(3,1,1)
-plot(info.TrainingLoss,'r') ;%绘制Trainloss图
-title('TrainLoss');
-ylabel('TrainingLossRate');
-xlabel('Iretation');
-subplot(3,1,2)
-plot(info.TrainingAccuracy,'b') ;%绘制Trainloss图
-title('TrainAccuracy');
-ylabel('TrainingLossRate');
-xlabel('Iretation');
-subplot(3,1,3)
-testactual=testImgs.Labels;
-confusionchart(testactual,testpreds);%绘制混淆矩阵,该函数使用时不能在其中含有运算步骤
-title('confusionchart');
-numCorrect=nnz(testpreds==testactual);
-fracCorrect=numCorrect/numel(testpreds)
-% 'ValidationData', imdsValidation,...%训练过程中验证，见test2.m
+save 'pipenet.mat' pipenet%将训练好的网络存储为net，以备调用
+%% 
+testSet = imageDatastore('testimage');
+testauds=augmentedImageDatastore([227,227],testSet);
+preds=classify(pipenet,testauds);
+size=numel(preds);
+for i=1:size
+    imshow(char(testSet.Files(i)))
+    title(['预测：' char(preds(i))])      
+end
